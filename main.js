00785889d104c0aee304459f79cca61deb9accc0
lhfdv <47636364+lhfdv@users.noreply.github.com>
2020-11-06 22:57:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+setInterval( function(){
+        var section = document.getElementsByTagName("section");
+        for (var i = 0; i<section.length; i++){
+            var trend = section[i];
+            if (trend.childElementCount !== 2){
+                continue;
+            }
+            if (trend.children[0].innerText.indexOf("トレンド") >= 0){
+                trend.style.display = "none";
+            }
+            if (trend.children[0].innerText.indexOf("Trending") >= 0){
+                trend.style.display = "none";
+            }
+        }
+    }
+);
