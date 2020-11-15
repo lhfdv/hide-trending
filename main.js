b0465edba58d56f20fc70ea51390b3c2ab1ecaac
lhfdv <47636364+lhfdv@users.noreply.github.com>
2020-11-14 22:17:01 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ setInterval( function(){
         var section = document.getElementsByTagName("section");
         for (var i = 0; i<section.length; i++){
             var trend = section[i];
-            if (trend.childElementCount !== 3){
+            if (trend.childElementCount !== 2){
                 continue;
             }
             if (trend.children[0].innerText.indexOf("トレンド") >= 0){
